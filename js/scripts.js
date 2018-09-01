6885db604cca016ebbab9a6d72b85260a57cdf6f
alex <androsaleksei@gmail.com>
2018-09-01 17:09:53 +0300

    *

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
+$(function () {
+
+    if($('.objects_populars').length){
+        var swiper = new Swiper('.objects_populars', {
+            slidesPerView: 4,
+            spaceBetween: 30,
+
+            navigation: {
+                nextEl: '.sl-next',
+                prevEl: '.sl-prev',
+            },
+
+            breakpoints: {
+                992: {
+                    slidesPerView: 3,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                450: {
+                    slidesPerView: 1,
+                },
+            }
+
+        });
+    }
+
+
+});
+
+
 function visibility(elements, link) {
     var link = $(link),
         elements = $(elements);
@@ -5,3 +36,5 @@ function visibility(elements, link) {
     elements.removeClass('d-none');
     link.remove();
 }
+
+
